f93f1408065f275aa7697324ce2f73d2599680b4
CarlosRojasF <crojasf@gmail.com>
2020-11-03 10:38:26 +0100

    aplicado estilos Tailwind directo al html Mobile first

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,25 @@ module.exports = {
       colors: {
         violet: 'hsl(257, 40%, 49%)',
       },
+      fontFamily: {
+        sans: [
+          '"Open Sans"',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+        headings: ['Poppins'],
+      },
       backgroundImage: {
         'body-sm': "url('../images/bg-mobile.svg')",
         'body-xl': "url('../images/bg-desktop.svg')",
